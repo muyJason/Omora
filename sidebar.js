e253808a1098d8b63e03166160aca8970c6e2992
muyJason <97745486+muyJason@users.noreply.github.com>
2025-08-05 14:28:25 +0200

    Add settings button to sidebar

--- sidebar.js
+++ sidebar.js
@@ -5,4 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
       parent.postMessage({ type: 'hide-sidebar' }, '*');
     });
   }
+
+  const settingsButton = document.getElementById('settings-btn');
+  if (settingsButton) {
+    settingsButton.addEventListener('click', () => {
+      chrome.tabs.create({ url: chrome.runtime.getURL('settings.html') });
+    });
+  }
 });
