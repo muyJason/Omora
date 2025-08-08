b16b9717fd864a2cf39eae825133550eb98baae4
muyJason <jason2002hg@gmail.com>
2025-08-08 12:47:55 +0200

    removed stuff

--- features/crunchyroll.js
+++
@@ -1,3 +0,0 @@
-export default function init(container) {
-  container.textContent = 'Custom settings for Crunchyroll are coming soon.';
-}
--- features/x.js
+++
@@ -1,3 +0,0 @@
-export default function init(container) {
-  container.textContent = 'Custom settings for X are coming soon.';
-}
