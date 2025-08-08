78cb9b80b0c80eea896597245c31a725d3c080a2
muyJason <97745486+muyJason@users.noreply.github.com>
2025-08-08 14:54:37 +0200

    Fix sidebar toggle and add dark theme

--- sidebar.js
+++ sidebar.js
@@ -3,8 +3,18 @@
   Promise.all([
     import(getURL('core/SidebarManager.js')),
     import(getURL('tools/index.js'))
-  ]).then(([core, tools]) => {
+  ]).then(async ([core, tools]) => {
+    window.omoraForceTheme = 'dark'
     const manager = new core.SidebarManager()
     tools.registerAllTools(manager)
+    manager.container.style.display = 'none'
+    chrome.runtime.onMessage.addListener(message => {
+      if (typeof message.sidebarVisible !== 'undefined') {
+        manager.container.style.display = message.sidebarVisible ? 'flex' : 'none'
+        if (!message.sidebarVisible) manager.closeActiveTool()
+      }
+    })
+    const { sidebarVisible = false } = await chrome.storage.local.get('sidebarVisible')
+    if (sidebarVisible) manager.container.style.display = 'flex'
   }).catch(() => {})
 })()
